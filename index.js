47cf548149d3582622b88c99c4e4531e22eeffb9
berkathy <katherine.berrios.garcia@gmail.com>
2024-04-11 21:32:51 -0400

    Cambios en index.js, eliminación de archivo index.html

--- index.js
+++ index.js
@@ -5,17 +5,14 @@ import moment from "moment";
 import _ from "lodash";
 import axios from "axios";
 
-// Generar un identificador único
-  uuidv4()
+// Arreglo de usuarios
+const usuarios = [];
+
+// Generando un identificador único
+  uuidv4();
 
 // Generando la fecha de registro
 moment().format("MMMM Do YYYY, h:mm:ss a");
-
-// Arreglo de números
-const numeros = [1, 2, 3, 4, 5, 6];
-
-//Método partition para mostrar números pares e impares por separado
-console.log(_.partition(numeros, (n) => n % 2));
 
 // Instancia axios
 // Datos de los usuarios consultados desde la API Random User
@@ -23,13 +20,27 @@ axios
   .get("https://randomuser.me/api/?results=12")
   .then((response) => {
     response.data.results.forEach((element) => {
-      const usuarios = (`Nombre: ${element.name.first} - Apellido: ${element.name.last} - ID: ${uuidv4().slice(30)} - Timestamp: ${moment()}`);
+      const name = element.name.first;
+      const surname = element.name.last;
+      const id = element.login.uuid.slice(30);
+      const timestamp = element.registered.date;
+      const gender = element.gender;
+      const usuario = {"Nombre": name, "Apellido": surname, "ID": id, "Timestamp": timestamp, "Genero": gender};
+
+      usuarios.push(usuario);
+      });
+
       console.log(
         // Mostrar los datos por consola en color azul y fondo blanco
-        chalk.blue.bgWhite.bold(usuarios)
+        chalk.blue.bgWhite.bold(
+          JSON.stringify(
+            //Método partition para mostrar usuarios de género masculino y fememnino por separado
+            _.partition(usuarios, ["Genero", "female"])
+          )
+        )
       );
-    });
-  })
+
+    })
   .catch((e) => {
   console.log(e);
   });
